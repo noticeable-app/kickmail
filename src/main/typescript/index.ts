06d363ace5383825239a54fa259d1085a8d8bd5f
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2022-09-04 22:05:20 +0200

    Add tests for the TS implementation

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,20 +1,29 @@
+import fetch from 'cross-fetch';
+
 const EMAIL_REGEXP =
     /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
 
-let DENY_LIST = new Set();
+export let DENY_LIST = new Set();
 
 async function loadDenyList(): Promise<Set<string>> {
     const denyList = await (await fetch('https://kickmail.pages.dev/denylist.txt')).text();
     const domains = denyList.split('\n');
     const domainSet = new Set(domains);
     for (const domain of domains) {
-        domainSet.add(domain);
+        if (domain.trim() !== '') {
+            domainSet.add(domain);
+        }
     }
     return domainSet;
 }
 
+async function init() {
+    return refresh();
+}
+
 async function refresh() {
     DENY_LIST = await loadDenyList();
+    return DENY_LIST;
 }
 
 /**
@@ -23,7 +32,7 @@ async function refresh() {
  * @returns True if the email is disposable, false otherwise.
  */
 function isDisposable(email: string): boolean {
-    return !DENY_LIST.has(email.split('@')[1]);
+    return !DENY_LIST.has(email.split('@')[2]);
 }
 
 /**
---
+++ src/test/typescript/index.spec.ts
@@ -0,0 +1,33 @@
+import {describe, expect, test} from '@jest/globals';
+import {DENY_LIST, isDisposable, isValid, refresh} from '../../main/typescript';
+
+describe('kickmail', () => {
+    test('DENY_LIST is empty if not initialized', async () => {
+        expect(DENY_LIST.size).toBe(0);
+    });
+
+    test('DENY_LIST is not empty after initialization', async () => {
+        await refresh();
+        expect(DENY_LIST.size).toBeGreaterThan(0);
+    });
+
+    test('DENY_LIST contains disposable email domain', async () => {
+        expect(DENY_LIST.has('sharklasers.com')).toBe(true);
+    });
+
+    test('disposable email is detected', async () => {
+        expect(isDisposable('qgm6ca+3avb6y3wziqbs@sharklasers.com')).toBe(true);
+    });
+
+    test('non disposable email pass detection', async () => {
+        expect(isDisposable('john.doe@acme.com')).toBe(false);
+    });
+
+    test('valid email address pass verification', async () => {
+        expect(isValid('john.doe@acme.com')).toBe(true);
+    });
+
+    test('invalid email address does not pass verification', async () => {
+        expect(isValid('john.doe@@acme.com')).toBe(false);
+    });
+});
