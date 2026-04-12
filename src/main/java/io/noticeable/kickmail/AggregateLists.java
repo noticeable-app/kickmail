bf9d6733387228bcb1d177e2ca6f516c87fe332f
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2026-04-12 14:43:41 +0200

    fix: update disposable-email-domains URL and remove defunct allowlist
    The upstream repo renamed master to main and removed allowlist.conf.

--- src/main/java/io/noticeable/kickmail/AggregateLists.java
+++ src/main/java/io/noticeable/kickmail/AggregateLists.java
@@ -7,9 +7,7 @@ import java.util.TreeSet;
 
 public class AggregateLists {
 
-    private static final String ALLOW_LIST_SOURCE1_URL = "https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/allowlist.conf";
-
-    private static final String DENY_LIST_SOURCE1_URL = "https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf";
+    private static final String DENY_LIST_SOURCE1_URL = "https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/refs/heads/main/disposable_email_blocklist.conf";
 
     private static final String DENY_LIST_SOURCE2_URL = "https://raw.githubusercontent.com/7c/fakefilter/main/txt/data.txt";
 
@@ -19,14 +17,12 @@ public class AggregateLists {
     public static void main(String[] args) throws IOException {
         final ListFetcher listFetcher = new ListFetcher();
 
-        final TreeSet<String> aggregatedAllowList = new TreeSet<>();
-        aggregatedAllowList.addAll(listFetcher.fetch(DENY_LIST_SOURCE1_URL));
-        aggregatedAllowList.addAll(listFetcher.fetch(DENY_LIST_SOURCE2_URL));
-        aggregatedAllowList.addAll(listFetcher.fetch(DENY_LIST_SOURCE3_URL));
-        aggregatedAllowList.removeAll(listFetcher.fetch(ALLOW_LIST_SOURCE1_URL));
-
+        final TreeSet<String> aggregatedDenyList = new TreeSet<>();
+        aggregatedDenyList.addAll(listFetcher.fetch(DENY_LIST_SOURCE1_URL));
+        aggregatedDenyList.addAll(listFetcher.fetch(DENY_LIST_SOURCE2_URL));
+        aggregatedDenyList.addAll(listFetcher.fetch(DENY_LIST_SOURCE3_URL));
         try (final BufferedWriter writer = new BufferedWriter(new FileWriter("denylist.txt"))) {
-            for (final String domain : aggregatedAllowList) {
+            for (final String domain : aggregatedDenyList) {
                 writer.write(domain);
                 writer.newLine();
             }
